6b1942f1132d0772ea89863ff21e570d292e73d9
Ravisankar Velayutham <>
2018-04-28 09:50:49 -0400

    first set of inclusions

--- firstSwiftProject/ViewController.swift
+++ firstSwiftProject/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var testLabel = "";
+    @IBOutlet weak var lblWwelcome: UILabel!
+    @IBOutlet weak var viewCtrlHolder: UIView!
+    @IBOutlet weak var txtUserName: UITextField!
+    @IBOutlet weak var txtPassWord: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
